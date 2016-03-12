143592fc7a5a796d6ebc2be969512854486ddf29
teejayx <teejayx@gmail.com>
2016-03-12 11:46:13 +0100

    refactoing in contorller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 	
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+	
 	def index
 		
 		@articles = Article.all
@@ -27,7 +29,7 @@ class ArticlesController < ApplicationController
 	  	end
 	  	
 	  	def edit 
-		  	@article = Article.find(params[:id])
+		  
 		  	
 		 
 		end 
@@ -35,13 +37,13 @@ class ArticlesController < ApplicationController
 	  	
 	  	
 	  	def show 
-		 @article = Article.find(params[:id]) 	
+	
 		  	
 		end 
 		
 		
 		def destroy
-			@article = Article.find(params[:id])
+			
 			@article.destroy
 			flash[:notice] = "Article was successfully deleted"
 			redirect_to articles_path
@@ -49,7 +51,7 @@ class ArticlesController < ApplicationController
 		
 		
 		def update 
-			@article = Article.find(params[:id])
+			
 			if @article.update(article_params) 
 				flash[:notice] = "Article was successfully updated"
 				redirect_to article_path (@article)
@@ -59,6 +61,14 @@ class ArticlesController < ApplicationController
 				 end 
 				
 	    end 
+		
+		
+		private 
+		 def set_article 
+		@article = Article.find(params[:id])
+			 
+	     end 
+		
 		
 	
 	private 

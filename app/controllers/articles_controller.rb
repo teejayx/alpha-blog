33e9445bf01b03d8ecbf7e16d77f97f7d6161d54
teejayx <teejayx@gmail.com>
2016-03-13 11:57:36 +0100

    styling flash and Messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 	  @article = Article.new (article_params)
 	  if @article.save
 		  
-		  flash[:notice] = "Article was succesfully created"
+		  flash[:success] = "Article was succesfully created"
 		 redirect_to article_path(@article)
 		  
 		  else 
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
 		def destroy
 			
 			@article.destroy
-			flash[:notice] = "Article was successfully deleted"
+			flash[:danger] = "Article was successfully deleted"
 			redirect_to articles_path
 		end
 		
@@ -53,7 +53,7 @@ class ArticlesController < ApplicationController
 		def update 
 			
 			if @article.update(article_params) 
-				flash[:notice] = "Article was successfully updated"
+				flash[:success] = "Article was successfully updated"
 				redirect_to article_path (@article)
 				
 				else 
